006d38d41ddddc274eedd432b140b6e292be1514
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2025-06-21 07:34:40 +0000

    Projects Page: Added  contented

---
+++ portfolio/src/components/Projects.js
@@ -0,0 +1,34 @@
+export default function Projects() {
+  return (
+    <section>
+      <h2>Projects</h2>
+      <ul>
+        <li>
+          <h3>Landing Page</h3>
+          <p>Personal landing page, built with HTML and SCC</p>
+          <a 
+            href="https://sue2023.github.io/LandingPage/" 
+            target="_blank" 
+            rel="noopener noreferrer"
+          >
+            View Project
+          </a>
+        </li>
+
+        <li>
+          <h3>Silchip International Limited</h3>
+          <p>Static web application for an electrical contracting company, built with HTML, CSS, and JavaScript.</p>
+          <a href="https://sue2023.github.io/Silchip_International_Limited/" target="_blank" rel="noopener noreferrer">
+            View Project
+          </a>
+        </li>
+	<li>
+          <h3>Portfolio</h3>
+          <p>Current page: personal portfolio built with React.</p>
+        </li>
+
+      </ul>
+    </section>
+  );
+}
+
