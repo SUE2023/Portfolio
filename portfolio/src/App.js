789ee1d0a1098ce873a1f0109bf6ae75f2afa2aa
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2025-06-21 05:46:57 +0000

    Update with routes: Header, Projects, Contact

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,26 @@
-import Photo from './Photo.jpg';
+//Professional Order of importing followed
+// 1. Third-party modules
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// 2. Styles
 import './App.css';
+// 3. Static assets
+import Photo from './Photo.jpg';
+// 4. Local components
+import Header from './components/Header';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={Photo} className="App-Photo" alt="Photo" />
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
