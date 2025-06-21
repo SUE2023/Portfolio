6d7cb3e6fa67b4cf07639a344ad81024906a402a
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2025-06-21 05:58:14 +0000

    About component content added

---
+++ portfolio/src/components/About.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>I’m a junior software engineer passionate about building web applications...</p>
+    </section>
+  );
+}
